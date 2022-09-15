d5d242e8a4168f5f110201fedb495f05ae29aa97
Kyuujay99 <113223164+Kyuujay99@users.noreply.github.com>
2022-09-15 23:17:30 +0700

    Add files via upload

---
+++ Volume Balok.py
@@ -0,0 +1,18 @@
+# Membuat Program Menghitung Bangun Ruang Balok Statis
+
+panjang = 20
+lebar = 7
+tinggi = 10
+
+Volume = panjang*lebar*tinggi
+print(Volume)   
+
+
+# Membuat Program Menghitung Bangun Ruang Balok Dinamis
+
+p = int(input("Masukkan Nilai Panjang : "))
+l = int(input("Masukkan Nilai Lebar : "))
+t = int(input("Masukkan Nilai Tinggi : "))
+
+V = p*l*t
+print(V)
---
+++ Volume Kubus.py
@@ -0,0 +1,13 @@
+# Membuat Program Menghitung Bangun Ruang Kubus Statis
+
+sisi = 10
+
+Volume = sisi*sisi*sisi
+print(Volume)
+
+
+# Membuat Program Menghitung Bangun Ruang Kubus Dinamis
+s = int(input("Masukkan Nilai Sisi : "))
+
+V = s*s*s
+print(V)
